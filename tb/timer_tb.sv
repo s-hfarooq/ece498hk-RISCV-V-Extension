5db330d5ac700b9037d81430949452a32bb73614
Hassan Farooq <1hassanfarooq@gmail.com>
2022-10-31 12:53:05 -0500

    Adding better print statements to timer_tb

--- tb/timer_tb.sv
+++ tb/timer_tb.sv
@@ -37,7 +37,7 @@ module timer_tb();
     endtask : reset
 
     task set_timer_val(int val);
-        $displayh("Setting timer to %p", val[31:0]);
+        // $displayh("Setting timer to %p", val[31:0]);
         timer_set_val <= val[31:0];
         set_timer <= 1'b1;
         ##1;
@@ -49,18 +49,21 @@ module timer_tb();
         reset();
         ##1;
 
-        for(int i = 0; i < 32'hFFFF_FFFF; i++) begin
+        for(int i = 1; i < 32'hFFFF_FFFF; i++) begin
             reset();
             ##1;
+
+            if(i % 1000 == 0)
+                $displayh("On iteration %p", i);
 
             set_timer_val(i);
 
             for(int j = 0; j < i; j++) begin
-                assert (timer_is_high == 1'b0) else $error("timer_is_high HIGH BEFORE IT SHOULD BE");
+                assert (timer_is_high == 1'b0) else $error("timer_is_high HIGH BEFORE IT SHOULD BE (i = %p)", i);
                 ##1;
             end
 
-            assert (timer_is_high == 1'b1) else $error("timer_is_high IS NOT HIGH WHEN IT SHOULD BE");
+            assert (timer_is_high == 1'b1) else $error("timer_is_high IS NOT HIGH WHEN IT SHOULD BE (i = %p)", i);
         end
 
         $display("Finished digital timer tests...");
